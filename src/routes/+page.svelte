863d3cc81ce4fbfdf1eedbdf2ffd7343a87b7d0d
Johnnie Hård <johnnie@synvinkel.org>
2023-08-23 09:20:31 +0200

    Refactor.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,37 +4,41 @@
 
 	let element: HTMLElement;
 
-	onMount(() => {
-		const scene = new THREE.Scene();
-		const camera = new THREE.PerspectiveCamera(
-			75,
-			window.innerWidth / window.innerHeight,
-			0.1,
-			1000
-		);
+	let scene: THREE.Scene;
+	let camera: THREE.PerspectiveCamera;
+	let renderer: THREE.Renderer;
+	let cube: THREE.Mesh;
 
-		const renderer = new THREE.WebGLRenderer();
+	onMount(() => {
+		scene = new THREE.Scene();
+		camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+		renderer = new THREE.WebGLRenderer();
 		renderer.setSize(window.innerWidth, window.innerHeight);
 		element.appendChild(renderer.domElement);
 
-		const geometry = new THREE.BoxGeometry(1, 1, 1);
-		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-		const cube = new THREE.Mesh(geometry, material);
+		cube = createCube();
 		scene.add(cube);
 
 		camera.position.z = 5;
 
-        function animate(){
-            requestAnimationFrame(animate)
-
-            cube.rotation.x += 0.01
-            cube.rotation.y += 0.01
-            
-            renderer.render(scene, camera)
-        }
-
-        animate()
+		animate();
 	});
+
+	function createCube() {
+		const geometry = new THREE.BoxGeometry(1, 1, 1);
+		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+		return new THREE.Mesh(geometry, material);
+	}
+
+	function animate() {
+		requestAnimationFrame(animate);
+
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.01;
+
+		renderer.render(scene, camera);
+	}
 </script>
 
 <div bind:this={element} />
